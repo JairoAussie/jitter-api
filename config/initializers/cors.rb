df4853ff92a0d2016bfcf09e10c9a62a5d50bba3
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2022-06-27 09:30:12 +1000

    add localhost to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://staging-jitter.netlify.app'
+    origins 'https://staging-jitter.netlify.app', 'http://localhost:3000'
     # origins '*'
     resource '*',
       headers: :any,
